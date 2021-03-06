6f9c955087acf4c07e6bdb3d9ce7703f85694259
Tom Wu <48511658+TomWuzedong@users.noreply.github.com>
2021-03-05 21:02:14 -0800

    Added comments in plotting_interactive.py

--- plotting_interactive.py
+++ plotting_interactive.py
@@ -5,6 +5,14 @@ import plotly.graph_objects as go
 
 
 def potential_to_age_interact(data):
+    """
+    This function uses the plotly package and creates an interactive
+    line plot and bar plot that shows the relationship between players' age
+    and the number of players with above-mean potential.
+    it involves a preprocessing step, which is grouping the dataset by age and
+    making a new dataframe whose columns are the age values and the number of
+    players with above-mean potential corresponding to each age.
+    """
     mean_potential = data['potential'].mean()
     high_data = data[data['potential'] >= mean_potential]
     by_age = high_data.groupby('age')['potential'].count()
@@ -13,8 +21,8 @@ def potential_to_age_interact(data):
            '# players have greater than or equal to avg potential': []}
     for i in range(16, 43, 1):
         dic['age'].append(i)
-        dic['# players have greater than or equal to avg potential'
-            ].append(by_age[i])
+        dic['# players have greater than or equal to avg \
+            potential'].append(by_age[i])
     new_data = pd.DataFrame(data=dic)
 
     fig = px.line(new_data, x='age',
@@ -30,27 +38,26 @@ def potential_to_age_interact(data):
 
 
 def srbp_interactive(data):
+    """
+    This function makes use of the preprocessed (by the srbp_data_cleaning
+    function in data_cleaning.py) position data and plots different skill
+    ratings (shooting, passing, dribbling, defending) by position (ST, CM, CB)
+    using bar plots, which are assigned into four subplots. The plots are
+    and they make use of the plotly package.
+    """
     fig = make_subplots(rows=2, cols=2)
     fig.add_trace(
         go.Bar(x=data['Position'], y=data['mean_shooting_rating'],
-               name='mean_shooting_rating'),
-        row=1, col=1
-    )
+               name='mean_shooting_rating'), row=1, col=1)
     fig.add_trace(
         go.Bar(x=data['Position'], y=data['mean_passing_rating'],
-               name='mean_passing_rating'),
-        row=1, col=2
-    )
+               name='mean_passing_rating'), row=1, col=2)
     fig.add_trace(
         go.Bar(x=data['Position'], y=data['mean_dribbling_rating'],
-               name='mean_dribbling_rating'),
-        row=2, col=1
-    )
+               name='mean_dribbling_rating'), row=2, col=1)
     fig.add_trace(
         go.Bar(x=data['Position'], y=data['mean_defending_rating'],
-               name='mean_defending_rating'),
-        row=2, col=2
-    )
+               name='mean_defending_rating'), row=2, col=2)
     fig.update_layout(height=600, width=800,
                       title_text="mean_skills_ratings_by_positions")
     fig.show()
